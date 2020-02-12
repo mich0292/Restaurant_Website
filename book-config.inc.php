1fb8681f412510c37895fcab334e29486a4f4aa6
gorden <60036275+gorden0929@users.noreply.github.com>
2020-02-13 00:35:33 +0800

    php

---
+++ book-config.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DBCONNECTTION','mysql:host=localhost;dbname=book');
+define('DBUSER','root');
+define('DBPASS','');
+
+>
---
+++ includes/signup.inc.php
@@ -0,0 +1,62 @@
+<?php
+    if(isset($_POST['signup-submit'])){
+        require 'dbh.inc.php';
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        
+        if(empty($username) || empty($password) || empty($email)){
+            header("Location: ../Registration.php?error=emptyfields&username=".$username."&email=".$email);
+            exit();
+        }
+        else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)){
+            header("Location: ../Registration.php?error=invalidemailusername");
+        }
+        else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            header("Location: ../Registration.php?error=invalidemail&username=".$username);
+            exit();
+        }
+        else if (!preg_match("/^[a-zA-Z0-9]*$/", $username)){
+            header("Location: ../Registration.php?error=invalidusername&email=".$email);
+            exit();
+        }
+        else{
+            $sql = "SELECT username FROM user WHERE username=?";
+            $stmt = mysqli_stmt_init($conn);
+            if(! mysqli_stmt_prepare($stmt, $sql)){
+                header("Location: ../Registration.php?error=sqlerror");
+                exit();
+            }
+            else{
+                mysqli_stmt_bind_param($stmt, "s", $username);
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_store_result($stmt);
+                $resultCheck = mysqli_stmt_num_rows($stmt);
+                if($resultCheck > 0){
+                    header("Location: ../Registration.php?error=usertaken&email=".$email);
+                    exit();
+                }
+                else{
+                    $sql = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+                    $stmt = mysqli_stmt_init($conn);
+                    if(! mysqli_stmt_prepare($stmt, $sql)){
+                        header("Location: ../Registration.php?error=sqlerror");
+                        exit();
+                    }
+                    else{
+                        $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+                        mysqli_stmt_bind_param($stmt, "sss", $username, $hashedPwd, $email);
+                        mysqli_stmt_execute($stmt);
+                        header("Location: ../Registration.php?signup=success");
+                        exit();
+                    }
+                }
+            }
+        }
+        mysqli_stmt_close($stmt);
+        mysqli_close($conn);
+    }
+    else{
+        header("Location ../Registration.php");
+        exit();
+    }
