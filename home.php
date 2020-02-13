f658d364476718e7d7e128af127b563c6474cba1
gorden <60036275+gorden0929@users.noreply.github.com>
2020-02-14 04:56:56 +0800

    navbar.php

--- home.php
+++ home.php
@@ -76,55 +76,7 @@
 	<div class="cover text-center" id="home-banner">
 		<!-- Nav bar -->
 		<!-- In reference to https://www.codeply.com/go/QAXbNGbWPA/bootstrap-4-navbar-transparent -->
-		<nav class="navbar navbar-toggleable-sm navbar-expand-lg navbar-inverse">
-			<div class="container-fluid py-0 justify-content-between">
-				<a class="navbar-brand" href="home.html">
-					<img class="img-fluid pr-3 pb-1 d-none d-lg-inline" src="images/home/logo2.png" alt="sushi-sama-logo" /><span class="px-2">Sushi Sama </span>
-				</a>
-				<button class="navbar-toggler btn" type="button" data-toggle="collapse"
-					data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-					aria-label="Toggle navigation">
-					<i class="fas fa-bars"></i>
-				</button>
-				<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-					<ul class="navbar-nav">
-						<li class="nav-item active px-2 mt-1">
-							<a class="nav-link" href="home.html">Home</a>
-						</li>
-						<li class="nav-item px-2 mt-1">
-							<a class="nav-link" href="menu.html">Menu</a>
-						</li>
-						<li class="nav-item px-2 mt-1">
-							<a class="nav-link" href="order.html">Order</a>
-						</li>
-						<li class="nav-item px-2 mt-1">
-							<a class="nav-link" href="career.html">Career</a>
-						</li>
-						<li class="nav-item px-2 mt-1">
-							<a class="nav-link" href="profile.html">Profile</a>
-						</li>
-						<li class="nav-item px-2 mt-1 d-lg-none">
-							<a class="nav-link" data-toggle="modal" data-target="#loginModal">Login	</a>
-						</li>
-						<li class="nav-item pl-2 d-none d-lg-block">
-                            <?php
-                                if(isset($_SESSION['userId'])){
-                                echo '<form action="includes/logout.inc.php" method="post">
-                                        <button type="submit" name="logout-submit" class="btn btn-outline-white btn-outline">Logout
-                                        </button>
-                                    <form>';
-                                }
-                                else{
-                                echo '<button class="btn btn-outline-white btn-outline" data-toggle="modal"
-                                        data-target="#loginModal"><i class="fa fa-user"></i> Login
-                                    </button>';
-                                }
-                            ?>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<?php include('navbar.php'); ?>
 		<!-- Homepage Banner -->
 		<div class="cover-container py-6">
 			<div class="cover-inner container justify-content-center">
