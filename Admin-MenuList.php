16986ddd4481b895002b99c60c7f0a9f9e182a7b
cutieangel34 <59465809+cutieangel34@users.noreply.github.com>
2020-02-18 01:59:56 +0800

    add mobile for menu + my menu sql

--- Admin-MenuList.php
+++ Admin-MenuList.php
@@ -127,81 +127,41 @@
       </table>
     </div>
     <div class="row justify-content-center rounded text-center bg-white mb-5 m-0 d-md-none">
-      <table class="table table-borderless table-responsive table-striped border m-2 text-left">
-        <colgroup>
-          <col class="p-1 px-2 w-25">
-          <col class="p-1 px-2 w-25">
-        </colgroup>
-        <tr>
-          <th scope="col">ID</th>
-          <td>1</td>
-        </tr>
-        <tr>
-          <th scope="col">Name</th>
-          <td>Salmon Sushi</td>
-        </tr>
-        <tr>
-          <th scope="col">Price</th>
-          <td>RM4.80</td>
-        </tr>
-        <tr>
-          <th scope="row">Action</th>
-          <td>
-            <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#dltMenuModal"><i class="fa fa-trash"></i></button>
-            <button class="btn btn-sm ml-0 btn-primary" data-toggle="modal" data-target="#editMenuModal"><i class="fa fa-edit"></i></button>
-          </td>
-        </tr>
-      </table>
-      <table class="table table-borderless table-striped border m-2 text-left">
-        <colgroup>
-          <col class="p-1 px-2 w-25">
-          <col class="p-1 px-2 w-25">
-        </colgroup>
-        <tr>
-          <th scope="col">ID</th>
-          <td>2</td>
-        </tr>
-        <tr>
-          <th scope="col">Name</th>
-          <td>California roll</td>
-        </tr>
-        <tr>
-          <th scope="col">Price</th>
-          <td>RM2.80</td>
-        </tr>
-        <tr>
-          <th scope="row">Action</th>
-          <td>
-            <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#dltMenuModal"><i class="fa fa-trash"></i></button>
-            <button class="btn btn-sm ml-0 btn-primary" data-toggle="modal" data-target="#editMenuModal"><i class="fa fa-edit"></i></button>
-          </td>
-        </tr>
-      </table>
-      <table class="table table-borderless table-striped border m-2 text-left">
-        <colgroup>
-          <col class="p-1 px-2 w-25">
-          <col class="p-1 px-2 w-25">
-        </colgroup>
-        <tr>
-          <th scope="col">ID</th>
-          <td>3</td>
-        </tr>
-        <tr>
-          <th scope="col">Name</th>
-          <td>Inari Igiri</td>
-        </tr>
-        <tr>
-          <th scope="col">Price</th>
-          <td>RM1.80</td>
-        </tr>
-        <tr>
-          <th scope="row">Action</th>
-          <td>
-            <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#dltMenuModal"><i class="fa fa-trash"></i></button>
-            <button class="btn btn-sm ml-0 btn-primary" data-toggle="modal" data-target="#editMenuModal"><i class="fa fa-edit"></i></button>
-          </td>
-        </tr>
-      </table>
+      <?php
+			foreach($menuList as $menuItem){ 
+				echo	'<table class="table table-borderless table-striped border md-3 text-left">
+						<colgroup>
+						  <col class="p-1 px-2 w-25">
+						  <col class="p-1 px-2 w-25">
+						</colgroup>
+						<tr>
+						  <th scope="col">ID</th>
+						  <td>'.$menuItem[0].'</td>
+						</tr>
+						<tr>
+						  <th scope="col">Category</th>
+						  <td>'.$menuItem[3].'</td>
+						</tr>
+						<tr>
+						  <th scope="col">Name</th>
+						  <td>'.$menuItem[1].'</td>
+						</tr>
+						<tr>
+						  <th scope="col">Price</th>
+						  <td>'.$menuItem[2].'</td>
+						</tr>
+						<tr>
+						  <th scope="col">Picture:<br>(Adult)</th>
+						  <td><img src="'.$menuItem[4].'" height="50" width="50"></td>
+						</tr>
+						<tr>
+						  <th scope="col">Action</th>
+						  <td><button class="btn btn-sm btn-danger deleteButton" data-toggle="modal" data-id="'.$menuItem[0].'"><i class="fa fa-trash"></i></button></td>
+						</tr>
+				</table>	';	
+			}
+		  ?>
+
     </div>
   </div>
   
